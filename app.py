b3938587a86677e7b1e842ad09075e8c4a4ced42
Oleks Litus <oleks.litus@gmail.com>
2023-05-29 22:37:22 -0400

    Bind server to envvar PORT

--- app.py
+++ app.py
@@ -1390,4 +1390,5 @@ def render_content(tab):
 # Run App
 # --------------------------------------------------------
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    port = os.environ.get['PORT', 8050]
+    app.run_server(debug=True, port=port)
